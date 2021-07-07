ee2b490e3392b35c42579e1a1e342805e104c72c
Allaye <kolade199@gmail.com>
2021-07-07 09:25:43 +0000

    added other properties to the implementation

--- Linked List/doubly_Linked_List.py
+++ Linked List/doubly_Linked_List.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 class Node:
@@ -12,7 +12,7 @@ class Node:
         self.prev = None
 
 
-# In[22]:
+# In[56]:
 
 
 class DoublyLinkedList:
@@ -22,16 +22,10 @@ class DoublyLinkedList:
         
     def append(self, data):
         new_node = Node(data)
-        cur = self.head
-        print(cur)
-        print(self.head)
-        cur = 10
-        print(cur)
-        print(self.head)
-        if(cur.data is None):
-            print('head')
-            cur = new_node
+        if(self.head.data is None):
+            self.head = new_node
             return
+        cur = self.head
         while(cur.next != None):
             cur = cur.next
         new_node.prev = cur
@@ -40,29 +34,51 @@ class DoublyLinkedList:
     
     def prepend(self, data):
         new_node = Node(data)
-        cur = self.head
-        if(cur is None):
-            cur = new_node
+        if(self.head.data is None):
+            self.head = new_node
             return
+        new_node.next = self.head
+        self.head.prev = new_node
+        self.head = new_node
+        new_node.prev = None
             
     def erase(self, index):
         pass
     
     def length(self):
-        pass
+        if(self.head.data is None):
+            print(f'Liked List is Empty')
+            return
+        enum, cur = [], self.head
+        while(cur.next is not None):
+            enum.append(cur.data)
+            cur = cur.next
+        enum.append(cur.data)
+        return len(enum)
     
     def display(self, display='P'):
         if(self.head.data is None):
+            print(f'Linked List is empty')
             return None
         enum, cur = [], self.head
         while(cur.next is not None):
-            cur = cur.next
             enum.append(cur.data)
+            cur = cur.next
+        enum.append(cur.data)
         if(display=='P'): print(enum)
         else: return enum
-        
     
     def get(self, index):
-        pass
+        if(index >= self.length()):
+            raise IndexError(f'Index provided is out of range of {self.length()}')
+            return
+        return self.display('R')[index]
+        
     
+
+
+# In[ ]:
+
+
+
 
