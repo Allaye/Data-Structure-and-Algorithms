94aa4b83d382e4cddb22639cc157f2a2f2631098
Allaye <kolade199@gmail.com>
2021-07-13 09:42:53 +0000

    basic recursion

--- Linked List/basic_recursion.py
+++ Linked List/basic_recursion.py
@@ -21,5 +21,6 @@ def word_split(phrase, list_of_word, output=None ):
         
     for word in list_of_word:
         if phrase.startswith(word):
-            
+            output.append(word)
+            return word_split(phrase[len(word):], list_of_word, output)
     
