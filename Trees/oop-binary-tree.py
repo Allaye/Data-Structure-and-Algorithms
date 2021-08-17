48f335ab4b74995a5bb90b63b4dba588b10950ef
Allaye <kolade199@gmail.com>
2021-08-17 08:27:21 +0000

    implementation of binary tree using oop

---
+++ Trees/oop-binary-tree.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Binary tree implementation using OOP
+
+
+# In[2]:
+
+
+class BinaryTree:
+    
+    def __init__(self, root):
+        self.root = root
+        self.left_node = None
+        self.right_node = None 
+    
+    def insert_left_node(self, new_node):
+        
+        if (self.left_node == None):
+            self.left_node = BinaryTree(new_node)
+        else:
+            bt = BinaryTree(new_node)
+            bt.left_node = self.left_node
+            self.left_node = bt
+    
+    def insert_right_node(self, new_node):
+        
+        if (self.right_node == None):
+            self.right_node = BinaryTree(new_node)
+        else:
+            bt = BinaryTree(new_node)
+            bt.right_node = self.right_node
+            self.right_node = bt
+    
+    def get_right_node(self):
+        return self.right_node
+    
+    def get_left_node(self):
+        return self.left_node
+    
+    def set_node(self, value):
+        self.root = value
+        
+    def get_root(self):
+        return self.root
+            
+        
+
+
+# In[ ]:
+
+
+
+
